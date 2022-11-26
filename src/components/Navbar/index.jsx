7b2e95daece4b23e13e7b5ef5ae33cc4cfd1de7d
Kira <kira.eva@protonmail.com>
2022-11-26 13:38:40 +0000

    Add github link to navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import "./index.scss";
 
 class Navbar extends React.Component {
@@ -11,6 +12,13 @@ class Navbar extends React.Component {
         <NavLink activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHome} />
         </NavLink>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/kira0x1/mikaela-app"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
       </div>
     );
   }
