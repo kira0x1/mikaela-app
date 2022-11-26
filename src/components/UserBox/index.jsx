9605e66b225c75da1d11c81482e6c0f0454d452a
Kira <kira.eva@protonmail.com>
2022-11-26 12:07:13 +0000

    improve user data boxes

--- src/components/UserBox/index.jsx
+++ src/components/UserBox/index.jsx
@@ -4,11 +4,19 @@ import "./index.scss";
 export default function UserBox({ user }) {
   return (
     <Link className="user-box">
-      <span className="user-name">{user.username}</span>
-      <br />
-      <span className="song-info">songs: {user.favorites.length}</span>
-      <br />
-      <span className="id-info">id: {user.id}</span>
+      <div className="user-name">{user.username}</div>
+      <div className="user-tag">
+        <span className="user-data-title">tag: </span>
+        {user.tag}
+      </div>
+      <div className="song-info">
+        <span className="user-data-title">songs: </span>
+        {user.favorites.length}
+      </div>
+      <div className="id-info">
+        <span className="user-data-title">id: </span>
+        {user.id}
+      </div>
     </Link>
   );
 }
